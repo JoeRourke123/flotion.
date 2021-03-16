7a9af2f262701827fb7b2e44df0c5a5f0d5c9431
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2021-03-16 19:44:43 +0000

    Populate statistics view with the data from endpoint

--- static/statistics.js
+++ static/statistics.js
@@ -1,5 +1,23 @@
-function loadStats() {
+async function loadStats() {
     let loader = document.getElementById("loading-window");
+
+    let statsRequest = await fetch("/s");
+    let stats = await statsRequest.json();
+
+    let totalCorrect = document.getElementById("total_corrects");
+    totalCorrect.innerText = stats["total_corrects"];
+
+    let totalCards = stats["total_cards"];
+    for(let level of ["red", "yellow", "green"]) {
+        let element = document.getElementById("total_" + level);
+        let totalLevel = stats["total_" + level];
+        element.innerText = `${totalLevel}/${totalCards}`;
+    }
+
+    let moduleRanking = document.getElementById("ranked_modules");
+    for(let mod of stats["ranked_modules"]) {
+        moduleRanking.innerHTML += `${mod}<br />`;
+    }
 
     setTimeout(() => {
         loader.classList.remove("is-loading");
