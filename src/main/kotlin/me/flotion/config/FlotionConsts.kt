414691ad0e5e82875a98a31f0dad13456ed526be
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2021-05-20 15:13:10 +0100

    Added some extra validation to the correct card mutation

--- src/main/kotlin/me/flotion/config/FlotionConsts.kt
+++ src/main/kotlin/me/flotion/config/FlotionConsts.kt
@@ -16,5 +16,7 @@ enum class ResponseMessages(val message: String) {
 	PERSONAL_WORKSPACE_ONLY("You can only use Flotion on a personal workspace - try authorising on a different workspace"),
 	INVALID_LIMITS("Oops - make sure the yellow limit is less than the green limit!"),
 	NOT_LOGGED_IN("This is awkward - you don't appear to be logged in."),
+	MALFORMED_CARD("Hmm - this card doesn't seem to match the format we were expecting! Maybe try recopying the template?"),
+	MISSING_CARD("We can't seem to find the card you specified - has it been deleted?"),
 	SUCCESS("OK")
 }
--- src/main/kotlin/me/flotion/mutation/CorrectCardMutation.kt
+++ src/main/kotlin/me/flotion/mutation/CorrectCardMutation.kt
@@ -12,19 +12,34 @@ import org.springframework.stereotype.Component
 
 @Component
 class CorrectCardMutation : Query {
-	class CorrectCardResponse(val response: Int = 200, val message: String = ResponseMessages.SUCCESS.message, val card: String? = null)
+	class CorrectCardResponse(
+		val response: Int = 200,
+		val message: String = ResponseMessages.SUCCESS.message,
+		val card: String? = null
+	)
 
 	@GraphQLDescription("marks a specified card as having been answered correctly")
-	suspend fun gotCorrect(card: String, context: NotionContext) : CorrectCardResponse {
+	suspend fun gotCorrect(card: String, context: NotionContext): CorrectCardResponse {
 		return if (context.user != null) {
 			val userToken: String = context.user.accessToken
 			val client: NotionClient = NotionSingleton.userClient(userToken)
 
-			val currentCorrect: Int = client.pages.getPage(card).propertyValues.first { it.name == CORRECT_PAGE_KEY }.value as Int
+			try {
+				// Find the current correct value in the card's page.
+				val currentCorrect: Int? =
+					client.pages.getPage(card).propertyValues.find { it.name == CORRECT_PAGE_KEY }?.value as Int?
 
-			client.pages.updatePage(card, PropertyValueList().number(CORRECT_PAGE_KEY, currentCorrect + 1))
+				if (currentCorrect == null) {
+					CorrectCardResponse(400, ResponseMessages.MALFORMED_CARD.message)
+				} else {
+					client.pages.updatePage(card, PropertyValueList().number(CORRECT_PAGE_KEY, currentCorrect + 1))
 
-			CorrectCardResponse(card = card)
+					// Successful response.
+					CorrectCardResponse(card = card)
+				}
+			} catch (exc: Exception) {
+				CorrectCardResponse(404, ResponseMessages.MISSING_CARD.message)
+			}
 		} else CorrectCardResponse(401, ResponseMessages.NOT_LOGGED_IN.message)
 	}
 }
