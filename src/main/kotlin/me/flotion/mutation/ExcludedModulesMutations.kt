d5a4b3536e85fe097a604fad53ec61513b67550c
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2021-05-21 23:53:32 +0100

    Added a description to the set excluded modules mutation

--- src/main/kotlin/me/flotion/mutation/ExcludedModulesMutations.kt
+++ src/main/kotlin/me/flotion/mutation/ExcludedModulesMutations.kt
@@ -1,5 +1,6 @@
 package me.flotion.mutation
 
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.server.operations.Mutation
 import me.flotion.config.ResponseMessages
 import me.flotion.context.NotionContext
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class ExcludedModulesMutations : Mutation {
+	@GraphQLDescription("sets the modules excluded for the user")
 	fun setModules(modules: List<String>, context: NotionContext): ModulesQuery.ModulesResponse {
 		if(context.user == null) return ModulesQuery.ModulesResponse(401, ResponseMessages.NOT_LOGGED_IN.message)
 
